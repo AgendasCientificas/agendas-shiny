082230c0aa8434a09f8035b0834fb46304d2ea31
unknown <giovannettipsi@gmail.com>
2025-11-05 13:37:24 -0300

    Actualizando diseño, sliders, etc. Falta terminar de poner lindo

--- ui.R
+++ ui.R
@@ -25,44 +25,54 @@ conicet <- read.csv("data/processed_data.csv")
 ui <- 
   navbarPage("",
              
-             tabPanel("Página principal",
-                      fluidPage(
-                        tags$head(
-                          tags$link(rel = "stylesheet", href = "https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"),
-                          tags$style(HTML(paste0("
-            body, html { font-family: 'Roboto', sans-serif; }
-            .titulo-app {
-              background-color: ", color_fondo_titulo, "; 
-              color: white;
-              font-weight: bold;
-              padding: 15px;
-              border-radius: 10px;
-              text-align: center;
-            }
-            .full-panel {
-              height: 100vh !important;
-              overflow-y: auto;
-            }
-          ")))
-                        ),
-                        
-                        div(
-                          class = "titulo-app",
-                          h1(
-                            style = "margin: 20px 0; font-family: 'Nimbus Sans', sans-serif; font-weight: bold;", 
-                            "Agendas científicas sobre desarrollo infantil: 
-                            relevamiento y análisis de datos abiertos del CONICET"
-                          )
-                        ),
-                        
-                        fluidPage(
+             
+#### Pestaña de página principal ####
+
+            tabPanel("Página principal",
+                     
+                     fluidPage(
+                       
+                       tags$head(
+                          # tags$link(rel = "stylesheet", href = "https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"),
+                          
+                          
+                          tags$style(
+                            HTML("
+                @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+            ")
+                          ),
+                          
+                          ),
+                       
+                       div(
+                         class = "titulo-app",
+                         h1(
+                           style = "margin: 20px 0; font-weight: bold;",
+                           "Agendas científicas sobre desarrollo infantil: 
+                           relevamiento y análisis de datos abiertos del CONICET"
+                           )
+                         ),
+                       
+                       fluidPage(
+                         
+                         tags$head(
+                           # tags$link(rel = "stylesheet", href = "https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"),
+                           
+                           
+                           tags$style(
+                             HTML("
+                @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+            ")
+                           ),
+                           
+                         ),
                           fluidRow(
                             # Primer recuadro
                             column(4, 
                                    div(
-                                     style = 'font-size: 18px; text-align: justify; background-color: #ABDDDE; padding: 10px; border-radius: 5px; margin: 15px 5px;', 
-                                     p(HTML("<strong>CONICET</strong> es el principal organismo de ciencia y tecnología del país. 
-                                            Conocer las temáticas de investigación de quienes lo integran y su localización geográfica e institucional
+                                     style = 'font-size: 18px; text-align: justify; background-color: #e1f7f7; padding: 10px; border-radius: 5px; margin: 15px 5px;', 
+                                     p(HTML("Somos un grupo de investigación perteneciente al <strong>CONICET</strong>, el principal organismo de ciencia y tecnología del país.<br><br>
+                                            Nos interesa conocer las temáticas de investigación de quienes lo integran y su localización geográfica e institucional
                                             constituye información valiosa para la exploración de agendas de investigación, 
                                             el establecimiento de vínculos interinstitucionales y de redes de investigadores. 
                                             Asimismo es un potencial insumo para elaboración de políticas científicas y la 
@@ -71,45 +81,72 @@ ui <-
                                    )
                             ),
                             
-                            # Segundo y Tercer recuadro unificados
+                            # Segundo recuadro
                             column(8, 
                                    div(
-                                     style = 'font-size: 18px; text-align: justify; background-color: #ABDDDE; padding: 10px; border-radius: 5px; margin: 15px 5px;', 
+                                     style = 'font-size: 18px; text-align: justify; background-color: #e1f7f7; padding: 10px; border-radius: 5px; margin: 15px 5px;', 
                                      p(HTML("Basándonos en las bases de datos recopiladas, analizamos el comportamiento de las becas e ingresos 
                                      otorgados por organismo en el periodo 2010-2020 a candidatos y candidatas con temas vinculados al desarrollo Niños, 
                                      Niñas y Adolescentes considerando asimismo su distribución geográfica y disciplinar. 
                                             Estos resultados fueron publicados en el año 2024 en la revista Iberoamericana de Ciencia, Tecnología y Sociedad (Smulski, et al. 2024).")),
                                      p(HTML("Aquí presentamos un tablero interactivo que pone a disposición la información recopilada, 
                                             sistematizada y analizada en dicha publicación con el fin de facilitar el acceso a la información."))
-                                   )
+                                   ),
+                                   
+                                   div(
+                                     style = 'font-size: 18px; text-align: justify; background-color: #f5f5f5; padding: 10px; border-radius: 5px; margin: 15px 5px; font-weight: bold;', 
+                                     p(HTML("Seleccionar parámetros de búsqueda")),
+                                     
+                                     sliderInput("yearInput",
+                                                 "Período de tiempo:",
+                                                 min = min(conicet$AÑO, na.rm = TRUE),
+                                                 max = max(conicet$AÑO, na.rm = TRUE),
+                                                 value = range(conicet$AÑO, na.rm = TRUE),
+                                                 width = "100%",
+                                                 sep = ""),
+                                     
+                                     shinyWidgets::pickerInput("disciplinaInput", 
+                                                               "Disciplina:",
+                                                               choices = unique(conicet$Nombre_comision),
+                                                               selected = unique(conicet$Nombre_comision),
+                                                               options = list(`actions-box` = TRUE),
+                                                               multiple = TRUE,
+                                                               width = "100%"
+                                                               )
+                                   
+
+                                   ),
+                                   
+                                   
+                                   
                             )
                           )
                         ),
+                       
+                       # Panel lateral
                         
-                        sidebarPanel(
-                          class = "full-panel",  # Añadir la clase para altura completa
-                          sliderInput("yearInput", "Seleccionar período de tiempo",
-                                      min = min(conicet$AÑO, na.rm = TRUE),
-                                      max = max(conicet$AÑO, na.rm = TRUE),
-                                      value = range(conicet$AÑO, na.rm = TRUE),
-                                      sep = ""),
-                          
-                          shinyWidgets::pickerInput("disciplinaInput", 
-                                                    "Seleccionar disciplina:", 
-                                                    choices = unique(conicet$Nombre_comision),
-                                                    selected = unique(conicet$Nombre_comision),
-                                                    options = list(`actions-box` = TRUE),
-                                                    multiple = TRUE,
-                          ),
-                          
-                          # Nube de palabras
-                          uiOutput("nubePalabras")  # Ajuste de altura sin scroll
-                        ),
+                        # sidebarPanel(
+                        #   class = "full-panel",  # Añadir la clase para altura completa
+                        #   # sliderInput("yearInput", "Seleccionar período de tiempo",
+                        #   #             min = min(conicet$AÑO, na.rm = TRUE),
+                        #   #             max = max(conicet$AÑO, na.rm = TRUE),
+                        #   #             value = range(conicet$AÑO, na.rm = TRUE),
+                        #   #             sep = ""),
+                        #   
+                        # 
+                        #   
+                        #   # Nube de palabras
+                            # Ajuste de altura sin scroll
+                        # ),
                         
-                        mainPanel(
+                        # mainPanel(
                           fluidRow(
-                            column(6, leafletOutput("mapa", height = "100vh")),  # Mapa a la izquierda
-                            column(6, 
+                            
+                            #Barplot de 
+                            column(4, uiOutput("nubePalabras")),
+                            
+                            column(4, leafletOutput("mapa", height = "100vh")),  # Mapa a la izquierda
+                            column(4, 
                                    fluidRow(
                                      column(12, plotOutput("graficoProyectosRegion", height = "50vh")),  
                                      column(12, 
@@ -126,8 +163,12 @@ ui <-
                                    )
                             )
                           )
-                        )
+                        # )
                       ),
+                     
+                     
+                     
+                     
                       
                       tags$footer(
                         HTML('<span style="font-size:12px;"><i>Autores: Florencia Altschuler, Federico Giovannetti, Fernando Steeb y Mariana Smulski. </i></span><br>
@@ -140,6 +181,11 @@ ui <-
                       
                       
              ),
+             
+             
+             
+#### Pestaña de tabla con datos ####
+             
              
              tabPanel("Datos",
                       HTML("<h2 style='font-size: 20px; font-weight: bold; color: #046C9A;'>Acá vas a poder acceder a los datos completos...</h2>"),
